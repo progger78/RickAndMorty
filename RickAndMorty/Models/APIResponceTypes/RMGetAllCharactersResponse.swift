71de5dde373aec6ffbf6aec617fd1a03bacffac9
Egor <pav_egor_24@mail.ru>
2023-08-26 07:18:17 +0300

    Added pagination logic with download indicatro footer

--- RickAndMorty/Models/APIResponceTypes/RMGetAllCharactersResponse.swift
+++ RickAndMorty/Models/APIResponceTypes/RMGetAllCharactersResponse.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct RMGetAllCharacterResponce: Codable {
+struct RMGetAllCharactersResponse: Codable {
     struct Info: Codable {
         let count: Int
         let pages: Int
